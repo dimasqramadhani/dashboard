a9fe93f1305148b40670a022b29580d09f9df23a
Dimasqi Ramadhani <dimasqiramadhani@gmail.com>
2025-05-17 14:41:54 +0800

    repair Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,6 +18,7 @@ import { SidebarTrigger, useSidebar } from "./ui/sidebar";
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const { toggleSidebar } = useSidebar();
+
   return (
     <nav className="p-4 flex items-center justify-between sticky top-0 bg-background z-10">
       {/* LEFT */}
@@ -25,13 +26,19 @@ const Navbar = () => {
       {/* <Button variant="outline" onClick={toggleSidebar}>
         Custom Button
       </Button> */}
+
       {/* RIGHT */}
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
+
         {/* THEME MENU */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon">
+            <Button
+              variant="outline"
+              size="icon"
+              aria-label="Toggle theme"
+            >
               <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
               <span className="sr-only">Toggle theme</span>
@@ -49,11 +56,12 @@ const Navbar = () => {
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
+
         {/* USER MENU */}
         <DropdownMenu>
-          <DropdownMenuTrigger>
+          <DropdownMenuTrigger asChild>
             <Avatar>
-              <AvatarImage src="/profile.jpg" />
+              <AvatarImage src="/profile.jpg" alt="User Avatar" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
           </DropdownMenuTrigger>
